5d580c86d91e17c84e1db5ccfa12eb4d1eec58f7
MUKIMOV <mukimov.shop@gmail.com>
2026-08-20 21:06:29 +0500

    Bugs Fixed

--- register.py
+++ register.py
@@ -1,8 +1,5 @@
 import db_connection
-<<<<<<< HEAD
 from hashing import hash_password, verify_password
-=======
->>>>>>> c8f6537813e323f84729d4dfb8fd17e18d252657
 
 def register():
     username = input("Username: ")
@@ -10,7 +7,6 @@ def register():
     email = input("Email: ")
     phone = input("Phone: ")
     
-<<<<<<< HEAD
     hashed_pwd = hash_password(password)
     
     try:
@@ -24,24 +20,11 @@ def register():
                 print("Успешная регистрация!")
     except Exception as e:
         print("Ошибка регистрации:", e)
-=======
-    with db_connection.conn.cursor() as cur:
-        try:
-            cur.execute(f"""
-                INSERT INTO Customers (username, password, email, phone_number, is_admin)
-                VALUES ('{username}', '{password}', '{email}', '{phone}', FALSE)
-            """)
-            db_connection.conn.commit()
-            print("Успешная регистрация!")
-        except Exception as e:
-            print("Ошибка регистрации:", e)
->>>>>>> c8f6537813e323f84729d4dfb8fd17e18d252657
 
 def login():
     username = input("Username: ")
     password = input("Password: ")
     
-<<<<<<< HEAD
     try:
         with db_connection.get_connection() as conn:
             with conn.cursor() as cur:
@@ -55,17 +38,6 @@ def login():
                     print("Неверный логин или пароль!")
     except Exception as e:
         print("Ошибка входа:", e)
-=======
-    with db_connection.conn.cursor() as cur:
-        cur.execute(f"SELECT id, username, password, email, phone_number, is_admin FROM Customers WHERE username='{username}' AND password='{password}'")
-        user = cur.fetchone()
-        
-        if user:
-            db_connection.current_user = user
-            print(f"\nДобро пожаловать, {user[1]}!")
-        else:
-            print("Неверный логин или пароль!")
->>>>>>> c8f6537813e323f84729d4dfb8fd17e18d252657
 
 def logout():
     db_connection.current_user = None
--- user_menu.py
+++ user_menu.py
@@ -11,7 +11,6 @@ def user_menu():
     print("[0] Logout")
     choice = input("Выберите пункт: ")
 
-<<<<<<< HEAD
     with db_connection.get_connection() as conn:
         with conn.cursor() as cur:
             match choice:
@@ -74,70 +73,3 @@ def user_menu():
 
                 case _:
                     print("Неверный пункт меню!")
-=======
-    with db_connection.conn.cursor() as cur:
-        match choice:
-            case "1":
-                cur.execute("SELECT id, food_name, price FROM Foods")
-                foods = cur.fetchall()
-                print("\n--- МЕНЮ ---")
-                for f in foods:
-                    print(f"[{f[0]}] {f[1]} — {f[2]} сомони")
-
-            case "2":
-                table_num = int(input("Номер столика: "))
-                customer_id = db_connection.current_user[0]
-                cur.execute(f"INSERT INTO Orders (customer_id, table_number) VALUES ({customer_id}, {table_num})")
-
-                cur.execute(f"SELECT MAX(id) FROM Orders WHERE customer_id = {customer_id}")
-                order_id = cur.fetchone()[0]
-                db_connection.conn.commit()
-
-                print(f"Заказ успешно создан! Ваш Order ID: {order_id}")
-
-            case "3":
-                order_id = int(input("Order ID: "))
-                food_id = int(input("Food ID: "))
-                qty = int(input("Количество: "))
-            
-                cur.execute(f"""
-                    INSERT INTO Order_Items (order_id, food_id, quantity) 
-                    VALUES ({order_id}, {food_id}, {qty})
-                """)
-            
-                cur.execute(f"""
-                    UPDATE Orders SET total_price = (
-                        SELECT SUM(f.price * oi.quantity)
-                        FROM Order_Items oi
-                        JOIN Foods f ON oi.food_id = f.id
-                        WHERE oi.order_id = {order_id}
-                    ) WHERE id = {order_id}
-                """)
-                db_connection.conn.commit()
-                print("Блюдо добавлено, общая сумма заказа пересчитана!")
-
-            case "4":
-                order_id = int(input("Order ID: "))
-                customer_id = db_connection.current_user[0]
-
-                cur.execute(f"SELECT order_date FROM Orders WHERE id={order_id} AND customer_id={customer_id}")
-                order = cur.fetchone()
-
-                if order:
-                    order_date = order[0].date()
-                    today = datetime.date.today()
-
-                    if order_date == today:
-                        cur.execute(f"UPDATE Orders SET status='Cancelled' WHERE id={order_id}")
-                        print("Заказ успешно отменён!")
-                    else:
-                        print("Отмена невозможна: заказ был сделан не сегодня.")
-                else:
-                    print("Заказ не найден или принадлежит не вам.")
-
-            case "0":
-                register.logout()
-
-            case _:
-                print("Неверный пункт меню!")
->>>>>>> c8f6537813e323f84729d4dfb8fd17e18d252657
